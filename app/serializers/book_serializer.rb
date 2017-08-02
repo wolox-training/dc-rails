cc3f46fcde96a40c1aa4d030c42533ad0f58c190
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-02 12:26:24 -0300

    add timestamp and add attributes to serializer

--- app/serializers/book_serializer.rb
+++ app/serializers/book_serializer.rb
@@ -1,3 +1,7 @@
 class BookSerializer < ActiveModel::Serializer
-  attributes :author, :title, :genere, :description
+  attributes :author, :title, :genere, :description, :publisher, :year, :image, :created_at, :updated_at
+
+  def image
+    { url: object.image }
+  end
 end
