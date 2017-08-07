a4b9c158a977e0d700d6f025c634a07e3e08676a
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-07 16:17:05 -0300

    add params.require

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,8 +5,17 @@ module Api
         render json: Rent.all, each_serializer: UserRentSerializer
       end
 
+      def rents_params
+        params.require(:rent).permit(:user_id, :book_id, :from, :to)
+      end
+
       def rents_create
-        new_rent = Rent.new(user_id: params[], book_id: params[], from: params[], to: params[])
+        new_rent = Rent.new(rents_params)
+        if new_rent.save
+          head :created
+        else
+          render json: new_rent.errors.as_json
+        end
       end
     end
   end
