59c86b9efd01e57ce24a6642cab0cca8fc4db636
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-08 13:01:22 -0300

    delete comment

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -2,7 +2,6 @@ module Api
   module V1
     class RentsController < ApiController
       def create
-        # new_rent = Rent.new(rents_params)
         new_rent = current_user.rents.build
         new_rent.assign_attributes(rents_params)
         if new_rent.save
