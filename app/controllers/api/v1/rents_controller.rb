a988e0f295729a0272bda1b41a06186ab14d78c0
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-08 12:52:02 -0300

    fix rubocop offences

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -1,7 +1,6 @@
 module Api
   module V1
     class RentsController < ApiController
-
       def create
         # new_rent = Rent.new(rents_params)
         new_rent = current_user.rents.build
@@ -14,6 +13,7 @@ module Api
       end
 
       private
+
       def rents_params
         params.require(:rent).permit(:book_id, :from, :to)
       end
