65b2c9ba0756feff5605986fac9918de5ff0394b
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-11 10:26:58 -0300

    fix rubocop offences

--- spec/factories/new_rent.rb
+++ spec/factories/new_rent.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :rent do
     user { create(:user) }
-    book { create( :book)}
+    book { create(:book) }
     from { Faker::Date.backward(1) }
     to { Faker::Date.forward(5) }
   end
