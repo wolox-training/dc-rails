f85d9bfbda53d0a75536c5060f5baa77106e2158
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-14 16:31:49 -0300

    create a suggestions route tests

---
+++ spec/controllers/api/v1/suggestions_controller_spec.rb
@@ -0,0 +1,50 @@
+require 'rails_helper'
+
+describe Api::V1::SuggestionsController do
+  include_context 'Authenticated User'
+  include Response::JSONParser
+
+  describe 'GET #index' do
+    context 'When fetching all the suggestions' do
+      let!(:suggestion) { create_list(:suggestion, 3) }
+
+      before do
+        get :index
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'responses with all suggestions json' do
+        expected = ActiveModel::Serializer::CollectionSerializer.new(
+          suggestion, each_serializer: SuggestionSerializer
+        ).to_json
+        expect(parsed_response_body.to_json) =~ JSON.parse(expected)
+      end
+    end
+  end
+
+  describe 'GET #show' do
+    context 'When fetching a suggestion' do
+      let!(:suggestions) { create_list(:suggestion, 3, user_id: user.id) }
+      let(:suggest) { suggestions.first }
+
+      before do
+        get :show, params: { id: suggest.id }
+      end
+
+      it 'responds with 200 status' do
+        expect(response).to have_http_status(:ok)
+      end
+
+      it 'responses with id suggestion json' do
+        expected = ActiveModel::Serializer.new(
+          suggest, serializer: SuggestionSerializer
+        ).to_json
+        expect(parsed_response_body.to_json) =~ JSON.parse(expected)
+        # binding.pry
+      end
+    end
+  end
+end
---
+++ spec/factories/new_suggestion.rb
@@ -0,0 +1,12 @@
+FactoryGirl.define do
+  factory :suggestion do
+    user { create(:user) }
+    author Faker::Book.author
+    link Faker::Internet.url
+    title Faker::Book.title
+    publisher Faker::Book.publisher
+    editorial Faker::Book.publisher
+    price Faker::Book.publisher
+    year Faker::Date.backward(14).year
+  end
+end
