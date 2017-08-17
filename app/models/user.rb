d9a7d5047467ed860483638efdfe4f8cde15ea34
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-17 12:14:22 -0300

    setup Google auth0

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, :omniauth_providers => [:google_oauth2]
   # Hooks
   before_validation :generate_verification_code, on: :create
   validates :first_name, :last_name, :email, :locale, presence: true
@@ -12,5 +13,18 @@ class User < ApplicationRecord
 
   def generate_verification_code
     self.verification_code = AuthenticableEntity.verification_code
+  end
+
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(:email => data["email"]).first
+
+    unless user
+      password = Devise.friendly_token[0,20]
+      user = User.create(first_name: data["name"], email: data["email"],
+        password: password, password_confirmation: password
+      )
+    end
+    user
   end
 end
