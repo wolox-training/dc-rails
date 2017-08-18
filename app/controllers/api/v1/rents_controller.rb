eefc2ee2f1988ddac8b1519227b37c27435d3f2d
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-18 11:47:41 -0300

    move create rent controlle to an interactor

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -4,23 +4,21 @@ module Api
       before_action :set_locale
 
       def create
-        new_rent = current_user.rents.build
-        new_rent.assign_attributes(rents_params)
+        rent = Rent.new(rents_params)
+        authorize rent
 
-        authorize new_rent
-
-        if new_rent.save
-          RentMailer.new_rent_notification(new_rent).deliver_later
-          head :created
+        rents = CreateRent.call(rents_params)
+        unless rents.success?
+          render json: { error:rents.message }
         else
-          render json: new_rent.errors.as_json
+          head :created
         end
       end
 
       private
 
       def rents_params
-        params.require(:rent).permit(:book_id, :from, :to)
+        params.require(:rent).permit(:user_id, :book_id, :from, :to)
       end
     end
   end
