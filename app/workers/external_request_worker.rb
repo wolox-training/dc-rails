20ee83698d012e4587bb8e6c10f7d33f426eaa25
dcarrilloWolox <david.carrillo@wolox.com.ar>
2017-08-24 15:57:03 -0300

    move sidekiq config to config/initializers

--- app/workers/external_request_worker.rb
+++ app/workers/external_request_worker.rb
@@ -1,21 +1,3 @@
-require 'sidekiq'
-require 'sidekiq-status'
-
-Sidekiq.configure_client do |config|
-  config.client_middleware do |chain|
-    chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes # default
-  end
-end
-
-Sidekiq.configure_server do |config|
-  config.server_middleware do |chain|
-    chain.add Sidekiq::Status::ServerMiddleware, expiration: 30.minutes # default
-  end
-  config.client_middleware do |chain|
-    chain.add Sidekiq::Status::ClientMiddleware, expiration: 30.minutes # default
-  end
-end
-
 class ExternalRequestWorker
   include Sidekiq::Worker
   include Sidekiq::Status::Worker
